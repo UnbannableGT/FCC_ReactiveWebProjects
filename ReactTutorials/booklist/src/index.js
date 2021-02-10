ae557fc1a172c1905869087e357f6e65a4080476
UnbannableGT <joey@joeyherring.com>
2021-02-10 17:31:34 -0600

    page updates

--- ReactTutorials/booklist/src/index.js
+++ ReactTutorials/booklist/src/index.js
@@ -3,37 +3,48 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/918sHoEOD8L._AC_UL200_SR200,200_.jpg',
+  title: 'Llama Llama I love You',
+  author: 'Anna Dewdney'
+}
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/41bChkOa4zL._AC_UL200_SR200,200_.jpg',
+  title: 'Think Again: The Power of Knowing What You Don\'t Know',
+  author: 'Adam Grant'
+}
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book 
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author} 
+      />
+      <Book 
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author} 
+      >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae veniam labore distinctio officia reiciendis, soluta voluptates sapiente aperiam molestias minima ex, tenetur amet commodi beatae? Velit asperiores ex nam fugiat!</p>
+      </Book >
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  const {img, title, author} = props;
+  console.log(props);
   return (
-  <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <article className="book">
+    <img className="book" src={img} />
+    <h1 className="book">{title}</h1>
+    <h4 className="book">{author}</h4>
+    {props.children}
   </article>
   )
 }
-
-const Image = () => {
-  return (
-    <img src="https://images-na.ssl-images-amazon.com/images/I/91NKthYHyZL._AC_UL200_SR200,200_.jpg" alt="" />
-  )
-}
-
-const Title = () => <h1>The Four Winds</h1>
-
-const Author = () => <h4 style={{color:'#617d98', fontSize: '0.75rem', marginTop: '.75rem'}}>Kristin Hannah</h4>
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
